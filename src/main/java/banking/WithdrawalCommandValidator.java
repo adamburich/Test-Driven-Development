26f03823abc7e06da6c2a44f18f16624bcfc8285
adamburich <adamburich@github.com>
2021-06-18 00:19:44 -0400

    - Minor changes for consolidation in WithdrawalCommandValidator

--- src/main/java/banking/WithdrawalCommandValidator.java
+++ src/main/java/banking/WithdrawalCommandValidator.java
@@ -16,21 +16,17 @@ public class WithdrawalCommandValidator {
         String amount_str = payload[1];
         withdrawal_target_is_int = !account_id.matches(Validator.DIGITS);
         withdrawal_amount_is_double = !amount_str.matches(Validator.DIGITS);
-        if (withdrawal_target_is_int && withdrawal_amount_is_double && (bank.getAccount(Integer.parseInt(account_id)) != null)) {
-            withdrawing_from_real_account = true;
-            Account account = bank.getAccount(Integer.parseInt(account_id));
-            if (account.getBalance() > 0) {
-                withdrawing_from_nonempty_account = true;
-                if (account instanceof SavingsAccount) {
-                    withdrawing_legal_amount = Double.parseDouble(amount_str) <= 1000;
-                    not_flagged = !((SavingsAccount) account).withdrawalUsed();
-                } else if (account instanceof CheckingAccount) {
-                    withdrawing_legal_amount = Double.parseDouble(amount_str) <= 400;
-                } else if (account instanceof CDAccount) {
-                    not_flagged = account.age() >= 12;
-                    withdrawing_legal_amount = Double.parseDouble(amount_str) >= account.getBalance();
-                }
-            }
+        withdrawing_from_real_account = withdrawal_target_is_int && withdrawal_amount_is_double && (bank.getAccount(Integer.parseInt(account_id)) != null);
+        Account account = bank.getAccount(Integer.parseInt(account_id));
+        withdrawing_from_nonempty_account = account.getBalance() > 0;
+        if (account instanceof SavingsAccount) {
+            withdrawing_legal_amount = Double.parseDouble(amount_str) <= 1000;
+            not_flagged = !((SavingsAccount) account).withdrawalUsed();
+        } else if (account instanceof CheckingAccount) {
+            withdrawing_legal_amount = Double.parseDouble(amount_str) <= 400;
+        } else if (account instanceof CDAccount) {
+            not_flagged = account.age() >= 12;
+            withdrawing_legal_amount = Double.parseDouble(amount_str) >= account.getBalance();
         }
         return withdrawal_target_is_int && withdrawal_amount_is_double && withdrawing_legal_amount && withdrawing_from_nonempty_account && withdrawing_from_real_account;
     }
